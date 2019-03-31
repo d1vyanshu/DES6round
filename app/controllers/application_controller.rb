a00d17d0bc00c20ae088299c9a97675fc802e1fc
Divyanshu <divyu@iitk.ac.in>
2019-03-31 12:21:45 +0530

    Finished app with full 16 round DES implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,36 @@
 class ApplicationController < ActionController::Base
+
+  def run
+    if params[:input].present?
+      if isvalid?
+        @python_output = `python lib/assets/py_script/code.py #{params[:input]}`
+        @python_output.upcase!
+      end
+        render 'new'
+    else
+      if !@python_output.nil?
+        @python_output = nil
+      end
+        render 'new'
+    end
+  end
+
+  private
+  def isvalid?
+    count=0
+    flag=0
+    params[:input].each_char do |c|
+      count+=1
+      if c.upcase<'A'&&c.upcase>'F'&&c<'0'&&c>'9'
+        flag=1
+      end
+    end
+
+    if count!=16||flag==1
+      flash[:danger] = "Check your input"
+      return false
+    else
+      return true
+    end
+  end
 end
